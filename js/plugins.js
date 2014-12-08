91ff4ecbd8ce70e85c3e2c4495f343bdda2c5da8
SEFarstad <sveinerikfarstad@Sveins-iMac-2.local>
2014-12-08 15:48:15 +0100

    Punctionation and 2x fix

--- js/plugins.js
+++ js/plugins.js
@@ -67,20 +67,11 @@ $(".accordion dd").on("click", "a:eq(0)", function (event)
         var dd_parent = $(this).parent();
 
         if(dd_parent.hasClass('active'))
-          $(".accordion dd div.content:visible").animate({
-            height: "toggle",
-            opacity: "toggle"
-        }, "slow");
+          $(".accordion dd div.content:visible").slideToggle("normal");
         else
         {
-          $(".accordion dd div.content:visible").animate({
-            height: "toggle",
-            opacity: "toggle"
-        }, "slow");
-          $(this).parent().find(".content").animate({
-            height: "toggle",
-            opacity: "toggle"
-        }, "slow");
+          $(".accordion dd div.content:visible").slideToggle("normal");
+          $(this).parent().find(".content").slideToggle("normal");
         }
       });
       
